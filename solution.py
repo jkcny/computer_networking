d1ff50e873cd27e475311278745b3d74e680a978
Jun <jake.jkchen@gmail.com>
2021-09-10 01:40:36 -0400

    update answers

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ def welcome_assignment_answers(question):
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
     	answer = "mTLS"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
@@ -21,9 +21,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
     	answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-    	answer = 3
+    	answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-    	answer = 2
+    	answer = 4
     return(answer)
 # Complete all the questions.
 
